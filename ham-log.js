39be700c42ae14281bf3300d5316b6b2c61d45c5
FroogalDerg <codecannamw@gmail.com>
2026-09-17 02:29:08 -0600

    Starting to add band plans

--- ham-log.js
+++ ham-log.js
@@ -6,8 +6,234 @@ let numberInputView = require("gui/number_input");
 let popupView = require("gui/popup");
 let dialogView = require("gui/dialog");
 let textBoxView = require("gui/text_box");
+let textInputView = require("gui/text_input");
 
-let prevView = [];
+let prevViews = [];
+
+// Create phonetic alphabet menu elements
+function phoneticAlphabetKeys() {
+    let keys = [];
+    // phoneticAlphabet.forEach(function(currentValue, index) {
+    //     keys.push(currentValue.word);
+    // });
+    for (let i = 0; i < phoneticAlphabet.length; i++) {
+        keys.push(phoneticAlphabet[i].word);
+    }
+    return keys;
+}
+
+function bandKeys(band) {
+    let keys = [];
+    for (let i = 0; i < band.length; i++) {
+        keys.push(band[i].wavelength.toString());
+    }
+    return keys;
+}
+
+// let mBandPlans = [
+//     {
+//         wavelength: 160,
+//         plans: [
+//             "CW",
+//             "Digital Modes",
+//             "CW QRP",
+//             "SSB, SSTV, and other wideband modes",
+//             "SSB QRP",
+//             "Experimental",
+//             "Beacons"
+//         ]
+//     },
+//     {
+//         wavelength: 80,
+//         plans: [
+//             "RTTY/Data DX",
+//             "RTTY/Data",
+//             "DX window",
+//             "SSTV",
+//             "AM calling frequency"
+//         ]
+//     },
+//     {
+//         wavelength: 60,
+//         plans: [
+//             "USB phone and CW/RTTY/data",
+//             "USB phone and CW/RTTY/data",
+//             "USB phone and CW/RTTY/data; 9.15 watts ERP max",
+//             "USB phone and CW/RTTY/data"
+//         ]
+//     },
+//     {
+//         wavelength: 40,
+//         plans: [
+//             "RTTY/data DX",
+//             "RTTY/data",
+//             "SSTV",
+//             "AM calling frequency",
+//         ]
+//     },
+//     {
+//         wavelength: 30,
+//         plans: [
+//             "RTTY",
+//             "Packet"
+//         ]
+//     },
+//     {
+//         wavelength: 20,
+//         plans: [
+//             "RTTY",
+//             "Packet",
+//             "NCDXF Beacons",
+//             "Packet",
+//             "SSTV",
+//             "AM calling frequency"
+//         ]
+//     },
+//     {
+//         wavelength: 10,
+//         plans: [
+//             "CW",
+//             "RTTY",
+//             "CW",
+//             "Beacons",
+//             "Phone",
+//             "SSTV",
+//             "AM",
+//             "Satellite Uplinks or Downlinks",
+//             "Repeater Inputs",
+//             "FM Simplex",
+//             "Repeater Outputs"
+//         ]
+//     },
+//     {
+//         wavelength: 6,
+//         plans: [
+//             "CW, beacons",                              // 1
+//             "beacon subband",                           // 2
+//             "SSB, CW",                                  // 3
+//             "DX window",                                // 4
+//             "SSB Calling",                              // 5
+//             "All modes",                                // 6
+//             "Nonvoice communications",                  // 7
+//             "Digital (packet) calling",                 // 8
+//             "Radio remote control (20-kHz channels)",   // 9
+//             "Pacific DX window",                        // 10
+//             "Repeater inputs (19 channels)",            // 11
+//             "Digital repeater inputs",                  // 12
+//             "Simplex (six channels)",                   // 13
+//             "Repeater outputs (19 channels)",           // 14
+//             "Digital repeater outputs",                 // 15
+//             "Repeater inputs (23 channels)",            // 16
+//             "FM simplex",                               // 17
+//             "TEST PAIR (input)",                        // 18
+//             "Repeater output (23 channels)",            // 19
+//             "Primary FM simplex",                       // 20
+//             "Secondary FM simplex",                     // 21
+//             "TEST PAIR (output)",                       // 22
+//             "Repeater inputs (except as noted; 19 channel)", // 23
+//             "Remote base FM simplex",                   // 24
+//             "Simplex",                                  // 25
+//             "Radio Remote Control",                     // 26
+//             "Repeater outputs (19 channels)",           // 27
+//             "Radio remote control",                     // 28
+//             "Simplex",                                  // 29
+//         ]
+//     },
+//     {
+//         wavelength: 2,
+//         plans: [
+//             "EME (CW)",
+//             "General CW and weak signals",
+//             "EME and weak-signal SSB",
+//             "National calling requency",
+//             "General SSB operation",
+//             "Propagation beacons",
+//             "New OSCAR subband",
+//             "Linear translator inputs",
+//             "FM repeater inputs",
+//             "Weak signal and FM simplex",
+//             "Linear translator outputs",
+//             "FM repeater outputs",
+//             "Misc and experimental modes",
+//             "OSCAR subband",
+//             "Repeater inputs",
+//             "Simplex",
+//             "National Simplex Calling Frequency",
+//             "Repeater outputs",
+//             "Repeater outputs",
+//             "Simplex",
+//             "Repeater inputs"
+//         ]
+//     },
+//     {
+//         wavelength: 1.25,
+//         plans: [
+//             "Weak-signal modes",
+//             "EME",
+//             "Propagation beacons",
+//             "SSB & CW calling frequency",
+//             "Weak-signal CW & SSB",
+//             "Local coordinator's option",
+//             "RM repeater inputs only",
+//             "FM simplex",
+//             "Digital, packet",
+//             "Links, control",
+//             "Local coordinators option",
+//             "Repeater outputs only"
+//         ]
+//     },
+// ];
+
+// function getMPlanByWavelength(wavelength) {
+//     for (let i = 0; i < mBandPlans.length; i++) {
+//         if (mBandPlans[i].)
+//     }
+// }
+
+// let cmBandPlans = [
+//     {
+//         wavelength: 70,
+//         plans: [
+
+//         ]
+//     }
+// ];
+
+let bands = {
+    hamMeterBands:   [
+        {wavelength: 630, bandStart: 472, bandEnd: 479, unit: "kHz"},
+        {wavelength: 160, bandStart: 1800, bandEnd: 479, unit: "kHz"},
+        {wavelength: 80, bandStart: 3.5, bandEnd: 4.0, unit: "MHz"},
+        {wavelength: 60},
+        {wavelength: 40, bandStart: 7.0, bandEnd: 7.3, unit: "MHz"},
+        {wavelength: 30, bandStart: 10.1, bandEnd: 10.15, unit: "MHz"},
+        {wavelength: 20, bandStart: 14.0, bandEnd: 14.35, unit: "MHz"},
+        {wavelength: 17, bandStart: 18.1, unit: "MHz"},
+        {wavelength: 15, bandStart: 21.0, bandEnd: 21.45},
+        {wavelength: 12, bandStart: 24.89, bandEnd: 24.99, unit: "MHz"},
+        {wavelength: 10, bandStart: 28.0, bandEnd: 30.0, unit: "MHz"},
+        {wavelength: 6, bandStart: 50.0, bandEnd: 54.0, unit: "MHz"},
+        {wavelength: 2, bandStart: 144.0, bandEnd: 148.0, unit: "MHz"},
+        {wavelength: 1.25, bandStart: 222.0, bandEnd: 225.0, unit: "MHz"},
+    ],
+    hamCentimeterBands: [
+        {wavelength: 70, bandStart: 420.0, bandEnd: 450, unit: "MHz"},
+        {wavelength: 33, bandStart: 902, bandEnd: 928, unit: "MHz"},
+        {wavelength: 23, bandStart: 1240, bandEnd: 1300, unit: "MHz"},
+        {wavelength: 13, bandStart: 2300, bandEnd: 2310, unit: "MHz"},
+        {wavelength: 12, bandStart: 2400, bandEnd: 2450, unit: "MHz"},
+        {wavelength: 9, bandStart: 3.3, bandEnd: 3.5, unit: "GHz"},
+        {wavelength: 5, bandStart: 5.65, bandEnd: 5.925, unit: "GHz"},
+        {wavelength: 3, bandStart: 10.0, bandEnd: 10.5, unit: "GHz"},
+        {wavelength: 1.25, bandStart: 24.0, bandEnd: 24.25, unit: "GHz"},
+    ],
+    hamMillimeterBands: [
+        {wavelength: 6.3, bandStart: 47.0, bandEnd: 47.2, unit: "GHz"},
+        {wavelength: 3.8, bandStart: 75.5, bandEnd: 81.0, unit: "GHz"},
+        {wavelength: 2.0, bandStart: 142.0, bandEnd: 149.0, unit: "GHz"},
+        {wavelength: 1.2, bandStart: 241.0, bandEnd: 250.0, unit: "GHz"},
+    ]
+};
 
 let phoneticAlphabet = [
     {letter: "A", word: "Alfa", sound: "AL FAH"},
@@ -37,18 +263,6 @@ let phoneticAlphabet = [
     {letter: "Y", word: "Yankee", sound: "YANG KEY"},
     {letter: "Z", word: "Zulu", sound: "ZOO LOO"},
 ];
-
-// Create phonetic alphabet menu elements
-function phoneticAlphabetKeys() {
-    let keys = [];
-    // phoneticAlphabet.forEach(function(currentValue, index) {
-    //     keys.push(currentValue.word);
-    // });
-    for (let i = 0; i < phoneticAlphabet.length; i++) {
-        keys.push(phoneticAlphabet[i].word);
-    }
-    return keys;
-}
 
 let qCodeMainDef = "Q-signals are a system of radio shorthand as old as wireless and developed from even older telegraphy codes. Q-signals are a set of\
 abbreviations for common information that save time and allow communication between operators who don’t speak a common language.\
@@ -122,7 +336,7 @@ let qCodeDef = [
 ];
 
 let views = {
-    mainMenu: submenuView.makeWith({
+    mainMenu: submenuView.makeWith({ // Main Menu
         header: "Ham Log"
     },
     [
@@ -130,87 +344,119 @@ let views = {
         "View",
         "Delete",
         "References",
+        "Profile",
         "Exit"
     ]),
-    refMenuView: submenuView.makeWith({
+    refMenuView: submenuView.makeWith({ // Reference Menu
         header: "References"
     },
     [
-        "Bandplans",
+        "Bands",
         "Q-Codes",
         "Phonetec Alphabet",
     ]),
-    qCodesMainView: submenuView.makeWith({
+    profileView: submenuView.makeWith({
+        header: "Profile"
+    },
+    [
+        "View",
+        "Set Callsign",
+        "Set License Level",
+    ]),
+    setCallsignView: textInputView.makeWith({
+        minLength: 4,
+        maxLength: 13,
+        header: "Set Callsign",
+        defaultText: "HAM Callsign",
+        defaultTextClear: true
+    }),
+    qCodesMainView: submenuView.makeWith({ // Q-Codes Main Menu
         header: "Q-Codes"
     }, [
         "View Q-Codes",
         "What are Q-Codes?"
     ]),
-    qCodesView: submenuView.makeWith({
+    qCodesView: submenuView.makeWith({ // Q-Codes Ref Menu
         header: "Q-Codes"
     }, qCodes),
-    phoneticAlphabetView: submenuView.makeWith({
+    phoneticAlphabetView: submenuView.makeWith({ // Phonetic Alphabet
         header: "Phonetic Alphabet"
     }, phoneticAlphabetKeys()),
-    frequencyInputView: numberInputView.makeWith({
-        header: "Frequency",
+    bandInputView: numberInputView.makeWith({ // Band Select View
+        header: "Select Band",
         defaultValue: 0,
         minValue: 0,
         maxValue: 700,
     }),
+    bandsView: submenuView.makeWith({
+        header: "Bands"
+    },
+    [
+        "Meter Bands",
+        "Centimeter Bands",
+        "Millimeter Bands"
+    ]),
+    mBandView: submenuView.makeWith({
+        header: "Meter Bands"
+    }, bandKeys(bands.hamMeterBands)),
+    cmBandView: submenuView.makeWith({
+        header: "Centimeter Bands"
+    }, bandKeys(bands.hamCentimeterBands)),
+    mmBandView: submenuView.makeWith({
+        header: "Millimeter Bands"
+    }, bandKeys(bands.hamMillimeterBands)), 
     popUp: dialogView.make(),
     longText: textBoxView.make(),
 };
-
-// Main menu
-eventLoop.subscribe(views.mainMenu.chosen, function(_sub, index, gui, eventLoop, views) {
-    if (index === 0) {
-        prevView.push(gui.viewDispatcher.currentView);
-        gui.viewDispatcher.switchTo(views.frequencyInputView);
-    } else if (index === 1) {
-        print("View Logs");
-    } else if (index === 2) {
-        print("Delete Log");
-    } else if (index === 3) {
-        prevView.push(gui.viewDispatcher.currentView);
-        gui.viewDispatcher.switchTo(views.refMenuView);
-    } else if (index === 4) {
-        eventLoop.stop();
-    }
-}, gui, eventLoop, views);
 
 // go to prev view if the back button is pressed, close app if back button is pressed from main menu
 eventLoop.subscribe(gui.viewDispatcher.navigation, function (_sub, _, gui, views) {
     if (gui.viewDispatcher.currentView === views.mainMenu) {
         eventLoop.stop();
     } else {
-        let index = prevView.length - 1;
+        let index = prevViews.length - 1;
     
-        gui.viewDispatcher.switchTo(prevView[index]);
-        let _ = prevView.splice(index);
+        gui.viewDispatcher.switchTo(prevViews[index]);
+        let _ = prevViews.splice(index);
     }
 }, gui, views);
 
 // Show references
 eventLoop.subscribe(views.refMenuView.chosen, function(_sub, index, gui, eventLoop, views) {
     if (index === 0) {
-        print("Bandplans");
+        print("Band");
+        prevViews.push(gui.viewDispatcher.currentView);
+        gui.viewDispatcher.switchTo(views.bandsView);
     } else if (index === 1) {
-        prevView.push(gui.viewDispatcher.currentView);
-        gui.viewDispatcher.switchTo(views.qCodesMainView);
+        print("Bandplan");
     } else if (index === 2) {
-        prevView.push(gui.viewDispatcher.currentView);
+        prevViews.push(gui.viewDispatcher.currentView);
+        gui.viewDispatcher.switchTo(views.qCodesMainView);
+    } else if (index === 3) {
+        prevViews.push(gui.viewDispatcher.currentView);
         gui.viewDispatcher.switchTo(views.phoneticAlphabetView);
     }
 }, gui, eventLoop, views);
 
+// Profile
+eventLoop.subscribe(views.profileView.chosen, function(_sub, index, gui, eventLoop, views) {
+    if (index === 0) {
+        print("Views")
+    } else if (index === 1) {
+        prevViews.push(gui.viewDispatcher.currentView);
+        gui.viewDispatcher.switchTo(gui.setCallsignView);
+    } else if (index === 2) {
+        print("Set license level")
+    }
+});
+
 // Q-Codes main submenu view
 eventLoop.subscribe(views.qCodesMainView.chosen, function(_sub, index, gui, eventLoop, views) {
     if (index === 0) {
-        prevView.push(gui.viewDispatcher.currentView);
+        prevViews.push(gui.viewDispatcher.currentView);
         gui.viewDispatcher.switchTo(views.qCodesView);
     } else if (index === 1) {
-        prevView.push(gui.viewDispatcher.currentView);
+        prevViews.push(gui.viewDispatcher.currentView);
         gui.viewDispatcher.switchTo(views.longText);
         views.longText.set("text", qCodeMainDef);
     }
@@ -219,17 +465,57 @@ eventLoop.subscribe(views.qCodesMainView.chosen, function(_sub, index, gui, even
 
 // Q-Codes ref menu
 eventLoop.subscribe(views.qCodesView.chosen, function(_sub, index, gui, eventLoop, views) {
-    prevView.push(gui.viewDispatcher.currentView);
+    prevViews.push(gui.viewDispatcher.currentView);
     views.longText.set("text", qCodeDef[index]);
     gui.viewDispatcher.switchTo(views.longText);
 }, gui, eventLoop, views);
 
 // Frequency Input
-eventLoop.subscribe(views.frequencyInputView.input, function(_sub, number, gui, views) {
-    prevView.push(gui.viewDispatcher.currentView);
+eventLoop.subscribe(views.bandInputView.input, function(_sub, number, gui, views) {
+    prevViews.push(gui.viewDispatcher.currentView);
     views.popUp.set("text", number.toString()); 
     gui.viewDispatcher.switchTo(views.popUp);
 }, gui, views);
+
+// Bands
+eventLoop.subscribe(views.bandsView.chosen, function(_sub, index, gui, eventLoop, views) {
+    if (index === 0) {
+        prevViews.push(gui.viewDispatcher.currentView);
+        gui.viewDispatcher.switchTo(views.mBandView);
+    } else if (index === 1) {
+        prevViews.push(gui.viewDispatcher.currentView);
+        gui.viewDispatcher.switchTo(views.cmBandView);
+    } else if (index === 2) {
+        prevViews.push(gui.viewDispatcher.currentView);
+        gui.viewDispatcher.switchTo(views.mmBandView);
+    }
+}, gui, eventLoop, views);
+
+// Meter bands
+// eventLoop.subscribe(views.mBandView, function(_sub, index, gui, eventLoop, views) {
+//     // prevViews.push(gui.viewDispatcher.currentView);
+//     // gui.viewDispatcher.switchTo(views.);
+// }, gui, eventLoop, views);
+
+// Main menu
+eventLoop.subscribe(views.mainMenu.chosen, function(_sub, index, gui, eventLoop, views) {
+    if (index === 0) {
+        prevViews.push(gui.viewDispatcher.currentView);
+        gui.viewDispatcher.switchTo(views.frequencyInputView);
+    } else if (index === 1) {
+        print("View Logs");
+    } else if (index === 2) {
+        print("Delete Log");
+    } else if (index === 3) {
+        prevViews.push(gui.viewDispatcher.currentView);
+        gui.viewDispatcher.switchTo(views.refMenuView);
+    } else if (index === 4) {
+        prevViews.push(gui.viewDispatcher.currentView);
+        gui.viewDispatcher.switchTo(views.profileView);
+    } else if (index === 5) {
+        eventLoop.stop();
+    }
+}, gui, eventLoop, views);
 
 gui.viewDispatcher.switchTo(views.mainMenu);
 eventLoop.run();
